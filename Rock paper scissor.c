62f9aed045659c990fd0459f1e36d2aba9083c89
Tommaso <alvintommaso@gmail.com>
2020-11-11 01:34:53 +0100

    Update Rock paper scissor.c

--- Rock paper scissor.c
+++ Rock paper scissor.c
@@ -1,6 +1,6 @@
 /*
  This code whas created by alvintommaso (on github).
- My source code is open source if you whant use in oter open source proget you can do this,
+ My source code is open source if you want use in oter open source proget you can do this,
  but tag my in the programmer list.
  You can't use my code to make money whiout my ok.
  Tank you!
