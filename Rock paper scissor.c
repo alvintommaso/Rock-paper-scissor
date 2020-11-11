b5d348b2f22d1438bf79878e386349ad7a55cf39
Tommaso <alvintommaso@gmail.com>
2020-11-11 01:06:41 +0100

    Update Rock paper scissor.c

--- Rock paper scissor.c
+++ Rock paper scissor.c
@@ -1,3 +1,12 @@
+/*
+ This code whas created by alvintommaso (on github).
+ My source code is public if you what use in oter open source proget you can do this,
+ but put my name in the code please.
+ You can't use my code to make money whiout my ok.
+ Tank you!
+*/
+
+
 #include <stdio.h>
 #include <time.h>
 #include <math.h>
